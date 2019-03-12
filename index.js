fa113b746d88947cf5169033276f0e374c5f3588
Nick Inman <ninman@Nicks-MBP.fios-router.home>
2019-03-12 11:34:34 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-/* Your code goes here */
+var customerName = 'bob'
+const leastFavoriteCustomer = 'not joe'
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+}
+
+function overWriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'joe'
+}
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = 'bob'
+  let favoriteCustomer = 'sometimes bob'
+}
